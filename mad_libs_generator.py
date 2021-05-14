f23db291cb5130c136331b464b873ba51762f23e
em-ariadne <82678022+em-ariadne@users.noreply.github.com>
2021-05-13 19:44:56 -0500

    Add files via upload

---
+++ mad_libs_generator.py
@@ -0,0 +1,34 @@
+
+word1 = (input("plural noun"))
+word2 = (input("adjective"))
+word3 = (input("food"))
+word4 = (input("another food"))
+word5 = (input("a part of your body"))
+word6 = (input("a noun"))
+word7 = (input("a verb + ing"))
+word8 = (input("a verb"))
+word9 = (input("a color"))
+word10 = (input("another color"))
+word11 = (input("an adjective"))
+word12 = (input("a noun"))
+word13 = (input("an adjective"))
+word14 = (input("an animal"))
+word15 = (input("adjective"))
+word16 = (input("another animal"))
+word17 = (input("a verb"))
+word18 = (input("an adjective"))
+word19 = (input("a verb"))
+word20 = (input("a noun"))
+
+
+print("Our " + word1 + " are packed for a hike in the " + word2 +
+      " Mountains! We are carrying a picnic lunch with " + word3 + " and "
+      + word4 + " on our " + word5 + ", and I have a " + word6 + " for "
+      + word7 + " photos. We will " + word8 + " pass meadows filled with "
+      + word9 + "and" + word10 + "flowers, and pass a" + word11 +
+      " waterfall that sounds like a " + word12 + ". Many animals live in the mountains, like the " + word13 + word14 +
+      " and "
+      + word15 + word16 + " It's fun to " + word17 + " them but important to be safe and keep a "
+      + word18 + " distance. The higher we " + word19 +
+      ",the smaller the wildflower meadow and waterfall appear; we may even see "
+      + word20 + " still on the ground from last winter ")
